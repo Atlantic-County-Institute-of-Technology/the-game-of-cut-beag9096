22ec74f6707f5b84f8f0392983cd4c735095a038
beag9096 <beag9096@student.acitech.org>
2025-11-03 12:54:16 -0500

    11/3 i tried, I'm lost

---
+++ src/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        arrDeck deck = new arrDeck();
+        boolean game = true;
+        while (game) {
+            int games = 0, player = 0, cpu = 0;
+
+        }
+
+    }
+}
